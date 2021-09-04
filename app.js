df1a3771afd9b7f30385da514b6d2ff3daa221e0
Gskar <77574975+Gskar@users.noreply.github.com>
2021-09-04 13:25:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const btn1 = document.querySelector(".button-1");
 const btn2 = document.querySelector(".button-2");
 
+let active = false;
+let active2 = false;
+
 btn1.addEventListener("click", () => {
-  btn1.classList.toggle("buttonActive");
+  if (active == false) {
+    btn1.classList.add("buttonActive");
+    btn1.textContent = "Active";
+    active = true;
+  } else {
+    btn1.classList.remove("buttonActive");
+    btn1.textContent = "Inactive";
+    active = false;
+  }
 });
 
 btn2.addEventListener("click", () => {
-  btn2.classList.toggle("buttonActive2");
+  if (active2 == false) {
+    btn2.classList.add("buttonActive2");
+    btn2.textContent = "Active";
+    active2 = true;
+  } else {
+    btn2.classList.remove("buttonActive2");
+    btn2.textContent = "Inactive";
+    active2 = false;
+  }
 });
